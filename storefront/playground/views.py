8c3a12a53319025899be9f4e2de5b44282357c39
JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>
2024-09-23 17:37:15 -0700

    Set up views and map urls to view
    Signed-off-by: JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+# Handle request and send response
+
+def say_hello(request):
+    return HttpResponse("hi, AI")
