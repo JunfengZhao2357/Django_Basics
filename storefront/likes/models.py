04abeb1209366f8410989e7585e076a60a09db7a
JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>
2024-09-24 20:21:23 -0700

    Define generic relationship
    Signed-off-by: JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>

--- storefront/likes/models.py
+++ storefront/likes/models.py
@@ -1,18 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 # Create your models here.
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-    
-    
-class TaggedItem(models.Model):
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
-    # tagged item can be any model in any app
-    # so this is a generic relationship
-    # To define a generic relationship. we need to define the following three fields.
+class LikedItem(models.Model):
+    user =  models.ForeignKey(User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
