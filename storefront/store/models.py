a054c3a3c8023098de529c67f0436431e208dcc3
JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>
2024-09-24 18:05:32 -0700

    Initialize two apps and build the data modeling
    Signed-off-by: JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>

---
+++ storefront/store/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    # if we don't explicitly write the primary key, 
+    # Django will automatically create an ID as the primary key.
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    # auto_now will update the time every time in model
+    last_update = models.DateTimeField(auto_now=True)
+
+  
+class Customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    # This is the choices field
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    
+
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+    # auto_now_add will create the timestamp automatically at the first time
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    
+
