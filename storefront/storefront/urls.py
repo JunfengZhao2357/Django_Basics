02f54220b035cc4a49fefef1a9e9dd57bf5da9ae
JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>
2024-09-26 15:35:47 -0700

    Set up the admin site
    Signed-off-by: JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>

--- storefront/storefront/urls.py
+++ storefront/storefront/urls.py
@@ -18,6 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
 
+# change the UI title and label
+admin.site.site_header = 'Storefront Admin'
+admin.site.index_title = 'Admin'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('playground/', include('playground.urls'))
