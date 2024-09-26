86fb999e3fccbe093f5092802bcfd29c28787488
JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>
2024-09-25 20:47:28 -0700

    Q object to realize the complex query
    Signed-off-by: JunfengZhao2357 <59008306+JunfengZhao2357@users.noreply.github.com>

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.http import HttpResponse
 from store.models import Product
 
@@ -7,9 +8,16 @@ from store.models import Product
 # Handle request and send response
 
 def say_hello(request):
-    query_set = Product.objects.all()
-    filtered_set = query_set.filter(unit_price__gt=20)
-    filtered_set_2 = query_set.filter(unit_price__range=(20,30))
+    # Products: inventory < 10 AND price < 20
+    # query_set = Product.objects.filter(inventory__lt=10, unit_price__lt=20)
+    # Products: inventory < 10 OR price < 20, we need to use Q object to realize 'OR'
+    query_set = Product.objects.filter(Q(inventory__lt=10) | Q(unit_price__lt=20))
+    # Q and: Q(inventory__lt=10) & Q(unit_price__lt=20)
+    # not Q: ~Q(inventory__lt=10)
+    
+    # filtered_set = query_set.filter(unit_price__gt=20)
+    # filtered_set_2 = query_set.filter(unit_price__range=(20,30))
+    
     # we can use 'pk' instead of 'id' to search for id=1 product
     # 'pk' stands for primary key and we can also use id=1 to search
     # product = Product.objects.get(pk=1)
@@ -20,4 +28,4 @@ def say_hello(request):
     # we can also filter the all query set 
     # eg, 
     # filtered_set = query_set.filter().filter().order_by()
-    return render(request, 'hello.html', {'name': 'Junfeng', 'products': list(filtered_set_2)})
+    return render(request, 'hello.html', {'name': 'Junfeng', 'products': list(query_set)})
